96d2ee5356e65e91871a1c1c35d58c58d0dd9289
Gustav Pettersson <gustav.b.pettersson@gmail.com>
2022-10-12 08:33:43 +0200

    Day 5

---
+++ AoC.Day5/JumpRunner.cs
@@ -0,0 +1,35 @@
+﻿using AoC.Common;
+
+namespace AoC.Day5;
+
+public class JumpRunner : IDay
+{
+    public int PartOne(string fileName)
+    {
+        List<int> jumps = InputReader.ReadLines(fileName).Select(x => int.Parse(x)).ToList();
+        return Run(jumps);
+
+    }
+
+    public int PartTwo(string fileName)
+    {
+        List<int> jumps = InputReader.ReadLines(fileName).Select(x => int.Parse(x)).ToList();
+        return Run(jumps, true);
+    }
+
+    private int Run(List<int> jumps, bool decreaseHighNumbers = false)
+    {
+        int i = 0;
+        int steps = 0;
+        while (i >= 0 && i < jumps.Count)
+        {
+            int j = i;
+            i = i + jumps[i];
+            if (decreaseHighNumbers && jumps[j] >= 3) jumps[j]--;
+            else jumps[j]++;
+            steps++;
+        }
+        return steps;
+    }
+
+}
---
+++ AoC17.Test/Day5Tests.cs
@@ -0,0 +1,24 @@
+﻿using AoC.Day5;
+
+namespace AoC17.Test;
+
+public class Day5Tests
+{
+    [Theory]
+    [InlineData("i5t1.txt", 5)]
+    [InlineData("i5.txt", 372671)]
+    public void PartOne_Tests(string input, int expected)
+    {
+        JumpRunner sut = new JumpRunner();
+        Assert.Equal(expected, sut.PartOne(input));
+    }
+
+    [Theory]
+    [InlineData("i5t1.txt", 10)]
+    [InlineData("i5.txt", 25608480)]
+    public void PartTwo_Tests(string input, int expected)
+    {
+        JumpRunner sut = new JumpRunner();
+        Assert.Equal(expected, sut.PartTwo(input));
+    }
+}
