23e94ab0373440f2cd97952ac0d81aaf50000340
Gustav Pettersson <gustav.b.pettersson@gmail.com>
2022-10-19 08:54:15 +0200

    Day6

---
+++ AoC.Day6/MemoryReallocator.cs
@@ -0,0 +1,107 @@
+﻿using AoC.Common;
+
+namespace AoC.Day6;
+
+public class MemoryReallocator : IDay
+{
+    private BlockMemory _blockMemory;
+    private List<string> _configs;
+    public int PartOne(string fileName)
+    {
+        var input = InputReader.ReadLines(fileName).ToList();
+        List<int> memory = input[0].Split("\t").Select(x => int.Parse(x)).ToList();
+        _blockMemory = new(memory);
+        _configs = new();
+        _configs.Add(_blockMemory.CurrentConfiguration());
+        int cycles = 0;
+        while (true)
+        {
+            cycles++;
+            int most = _blockMemory.MostBlocks();
+            string conf = _blockMemory.Redistribute(most);
+            if (_configs.Contains(conf))
+            {
+                _configs.Add(conf);
+                break;
+            }
+            else _configs.Add(conf);
+        }
+
+        return cycles;
+    }
+
+    public int PartTwo(string fileName)
+    {
+        PartOne(fileName);
+        string expectedConfig = _configs.Last();
+        int cycles = 0;
+        while (true)
+        {
+            cycles++;
+            int most = _blockMemory.MostBlocks();
+            string conf = _blockMemory.Redistribute(most);
+            if (conf == expectedConfig) break;
+        }
+
+        return cycles;
+    }
+
+
+}
+
+public class BlockMemory
+{
+    private readonly List<int> _memory;
+
+    public BlockMemory(List<int> memory)
+    {
+        _memory = memory;
+    }
+
+    public int MostBlocks()
+    {
+        int index = 0;
+        int most = -1;
+        for (int i = 0; i < _memory.Count; i++)
+        {
+            if (_memory[i] > most)
+            {
+                most = _memory[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public string Redistribute(int index)
+    {
+        int v = _memory[index];
+        _memory[index] = 0;
+        int i = index + 1;
+        while (v > 0)
+        {
+            try
+            {
+                _memory[i]++;
+                v--;
+            }
+            catch (Exception)
+            {
+                i = i - _memory.Count;
+                _memory[i]++;
+                v--;
+            }
+            finally
+            {
+                i++;
+            }
+        }
+
+        return CurrentConfiguration();
+    }
+
+    public string CurrentConfiguration()
+    {
+        return string.Join(",", _memory.ToArray());
+    }
+}
---
+++ AoC17.Test/Day6Tests.cs
@@ -0,0 +1,24 @@
+﻿using AoC.Day6;
+
+namespace AoC17.Test;
+
+public class Day6Tests
+{
+    [Theory]
+    [InlineData("i6t1.txt", 5)]
+    [InlineData("i6.txt", 3156)]
+    public void PartOne_Tests(string input, int expected)
+    {
+        MemoryReallocator sut = new MemoryReallocator();
+        Assert.Equal(expected, sut.PartOne(input));
+    }
+
+    [Theory]
+    [InlineData("i6t1.txt", 4)]
+    [InlineData("i6.txt", 1610)]
+    public void PartTwo_Tests(string input, int expected)
+    {
+        MemoryReallocator sut = new MemoryReallocator();
+        Assert.Equal(expected, sut.PartTwo(input));
+    }
+}
